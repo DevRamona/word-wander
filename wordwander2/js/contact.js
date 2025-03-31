849281406eeff58f7de4cb0d1b1e556a6f09e319
DevRamona <r.ingabire@alustudent.com>
2025-03-31 16:19:01 +0200

    feat: add the contacts page

---
+++ wordwander2/js/contact.js
@@ -0,0 +1,31 @@
+function initMap() {
+    const location = { lat: YOUR_LAT, lng: YOUR_LNG };
+    const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 15,
+        center: location,
+        styles: [
+            {
+                "featureType": "all",
+                "elementType": "geometry",
+                "stylers": [{"color": "#f8f7fe"}]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [{"color": "#8a2be2"}]
+            }
+        ]
+    });
+
+    const marker = new google.maps.Marker({
+        position: location,
+        map: map,
+        icon: {
+            url: 'images/map-marker.svg',
+            scaledSize: new google.maps.Size(40, 40)
+        }
+    });
+}
+
+// Initialize map when the page loads
+window.addEventListener('load', initMap);
